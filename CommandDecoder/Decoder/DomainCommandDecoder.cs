8bf70d39fac6203d8c44bbc377c7dce9d63024f0
Jeb Feng <ranzeplay@outlook.com>
2022-09-18 16:33:43 +0800

    feat (decoder) : Add domain command decoder

---
+++ CommandDecoder/Decoder/DomainCommandDecoder.cs
@@ -0,0 +1,36 @@
+﻿using CommandDecoder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandDecoder.Decoder
+{
+    internal class DomainCommandDecoder
+    {
+        public static DecodeResult CreateDomainCommand(byte[] stream, PackageMetadata metadata, int baseIndex)
+        {
+            stream = stream[baseIndex..];
+
+            return new(1, new CommandTableEntry
+            {
+                Location = baseIndex,
+                RawData = stream[0..1],
+                Description = "Create domain"
+            });
+        }
+
+        public static DecodeResult DestroyDomainCommand(byte[] stream, PackageMetadata metadata, int baseIndex)
+        {
+            stream = stream[baseIndex..];
+
+            return new(1, new CommandTableEntry
+            {
+                Location = baseIndex,
+                RawData = stream[0..1],
+                Description = "Destroy domain"
+            });
+        }
+    }
+}
